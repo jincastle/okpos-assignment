fc4717d17a7e24e18d553fd2b9fb35fbab8b2f66
jincastle <dksk0101@gmail.com>
2025-08-25 14:51:19 +0900

    doc url redoc로 설정

--- okpos_assignment/urls.py
+++ okpos_assignment/urls.py
@@ -4,7 +4,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Swagger 문서화 설정
+# 문서화 설정
 schema_view = get_schema_view(
     openapi.Info(
         title="OKPOS Assignment API",
@@ -20,5 +20,5 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     
     # API 문서화
-    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-swagger-ui'),
 ]
