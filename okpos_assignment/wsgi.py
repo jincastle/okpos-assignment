4d9012bd97d261780793a58ab016de5364f171a0
jincastle <dksk0101@gmail.com>
2025-08-25 10:55:01 +0900

    환경설정

---
+++ okpos_assignment/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for okpos_assignment project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'okpos_assignment.settings')
+
+application = get_wsgi_application()
